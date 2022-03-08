ff5205c31422fd5de467d452372caec40fcd1735
sonalizine <sonali.zine23@gmail.com>
2022-03-08 16:30:02 +0530

    InvestmentBanking

---
+++ src/test/java/testClasses/TC01_Login.java
@@ -0,0 +1,12 @@
+package testClasses;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC01_Login {
+	@Test
+	public void TC01() {
+		Reporter.log("Testcase 1 passed");
+	}
+
+}
